f81a60e9960e11fe663bc735b84e236d2ff9c95f
Juan <johnstar08082@gmail.com>
2025-04-02 15:45:12 -0500

    fin clase siu

--- April2.py
+++ April2.py
@@ -1,13 +1,46 @@
 #el historial de este script se lee de abajo para arriba
+#como hacer anotaciones: SHIFT+2
 #Importar librerias
+#para no usar sufijos puedes usar "from math import *" pero no es recomendable porque se confunden las llamadas
+import math as m
 
-#Como redondear
+#Programa que lee un numero entero de 3 digitos y que lo imprima alreves
+rn=int(input("Introduce un numero de 3 digitos→"))
+x= rn%10
+xx= (rn//10)%10
+xxx= (rn//10//10)%10
+print("Numero Invertido:",100*x+10*xx+xxx)
+
+#Programa de  hallar distancia entre 2 puntos
+print("Punto 1:(_;_)")
+P1x=float(input("Introduce la abcisa de un punto→"))
+print("Punto 1:(",P1x,";","_")
+P1y=float(input("Introduce la ordenada de un punto→"))
+print("Punto 1:(",P1x,";",P1y,")")
+P2x=float(input("Introduce la abcisa de otro punto→"))
+print("Punto 1:(",P1x,";",P1y,")")
+print("Punto 2:(",P2x,";","_")
+P2y=float(input("Introduce la ordenada de otro punto→"))
+print("Punto 1:(",P1x,";",P1y,")")
+print("Punto 2:(",P2x,";",P2y,")")
+distancia = m.sqrt((P1x-P2x)**2+(P1y-P2y)**2)
+print("La distancia entre los 2 puntos que has introducido es:",distancia)
+
+"""print("Raiz cuadrada:",m.sqrt(16))
+print("Factorial:",m.factorial(10))
+print("coseno en grados:",m.cos(16))
+print("coseno en rad:",m.cos(m.radians(60)))
+print("log base 2:",m.log2(64))
+print("log natural:",m.log(64))"""
+
+#como hacer anotaciones: SHIFT+2
+"""#Como redondear
 x = 3.4873652378284569
 print("Redondeado con 2 decimales",round(x,2))
 print("Redondeado con 3 decimales",round(x,3))
-print("Redondeado con 4 decimales",round(x,4))
+print("Redondeado con 4 decimales",round(x,4))"""
 
-#juego del patito?
+#juego del patito
 kg = float(input("Introduzca su peso en gramos→"))
 m = float(input("Ahora introduzca su altura en centimetros→"))
 IMC = (kg/1000)/(m/100)**2
