7a7672d8fe16fe2190291e16b2cef84c404653ea
Juan <johnstar08082@gmail.com>
2025-04-02 14:25:56 -0500

    my backup

---
+++ April2.py
@@ -0,0 +1,9 @@
+#el historial de este script se lee de abajo para arriba
+#
+
+#programa que permita hallar volumen de una esfera
+r = float(input('Introduce el radio de la esfera:'))
+Area = 4*3.14159265359*r**2
+Volume= (4/3)*3.14159265359*r**3
+print("Area de tu esfera→", Area)
+print("Vólumen de tu esfera→", Volume)
