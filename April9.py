6dcab2fbc67db62bfcac62f13be575f1ed35a457
Juan <johnstar08082@gmail.com>
2025-04-09 14:40:37 -0500

    inicio clase siu

---
+++ April9.py
@@ -0,0 +1,26 @@
+#el historial de este script se lee de abajo para arriba
+
+#ay un kahoot que miedo
+
+#ejemplos de and, or, not
+print("true and false?",True and False)
+print("true and true?",True and True)
+print("true or false?",True or False)
+print("false or false?",False or False)
+print("not true?",not True)
+print("4<10",4<10)
+print("4<=10",4<=10)
+print("4>=5",4>=5)
+print("4==4",4==4)
+print("4!=4",4!=4)
+print("4<5<8",4<5<8)
+print("casa"<"salon") #esto hace segun el orden alfabetico
+print("casa"<"sol")
+print((2<5)+(5<10))
+print(2+4<10)
+
+edad=30
+condicion = edad
+msg1= condicion*"Menorsito"
+msg2=(not condicion)*"Mayorsito"
+print(msg1+msg2)
