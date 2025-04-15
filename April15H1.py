4ed5579b072bb77926f83e01edb576d39b8d4a08
Juan <johnstar08082@gmail.com>
2025-04-15 12:23:06 -0500

    12:22

---
+++ April15H1.py
@@ -0,0 +1,18 @@
+import math
+
+
+def pregunta_1(radio: float) -> float:
+    PI = 3.1416
+    return None
+
+
+def pregunta_2(a: float, b: float) -> float:
+    return None
+
+
+def pregunta_3(cadena: str) -> int:
+    return None
+
+
+def pregunta_4(n: int) -> int:
+    return None
