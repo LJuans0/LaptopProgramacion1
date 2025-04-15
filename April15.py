5087a37226e6b9b893555afdfb6cac49b448326d
Juan <johnstar08082@gmail.com>
2025-04-15 12:03:48 -0500

    12:03

---
+++ April15.py
@@ -0,0 +1,26 @@
+#Historial de abajo para arriba ejq2oiejqiojdaiod
+#hoy se aprende a usar while buey
+
+#programa que halla la suma de todos los numeros pares desde el 2 hasta el dato que introduzca el usuario
+num4= int(input("pon tu numero prro:"))
+if num4<2:
+    print("intorudce un numero mayor o igual a 2")
+while num4
+#programa que imprime todos los numeros multiples de 5 desde el 50 al 55
+"""num2=50
+while num2 <= 550:
+    print(num2)
+    num2 +=5"""
+
+#Programa que imprima numeros del 100 al 80, disminuyendo de 2 en 2
+"""num1=100
+while num1>=80:
+    print(num1)
+    num1 -= 2"""
+
+#Programa que imprima numeros del 1 al 7
+#<>
+"""num0=0
+while num0<7:
+    num0+=1
+    print(num0)"""
