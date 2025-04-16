54125c0b461ff969512fad47a16b1616d363a877
Juan <johnstar08082@gmail.com>
2025-04-16 15:02:27 -0500

    14:22

--- April16.py
+++ April16.py
@@ -1,1 +1,48 @@
 #historial de abajo para arriba
+
+
+
+#programa que permita leer como dato unnumero y el programa imprima la tabla de multiplicardel numero que se ha ingresado con ese dato
+nn0=int(input("Tabla de multiplicacion de este numero:"))
+nn0r=1
+while nn0r<=10:
+    print(nn0,"x",nn0r, "=", nn0*nn0r)
+    nn0r += 1
+
+#programa que imprima los 15 numeros naturales que son multiples de 7
+"""nn=1
+nn2=0
+while nn<=15:
+    nn2+=7
+    print(nn, "-", nn2)
+    nn += 1"""
+
+#programa que imprima 7 veces utec
+"""numeror=1
+while numeror <=7:
+    numeror +=1
+    print("UTEC")"""
+
+#programa que imprime 9,8,7,6,5,4
+"""numerouniversal=9
+while numerouniversal >=4:
+    print(numerouniversal, end="")
+    numerouniversal -=1"""
+
+#programa que imprime 5,10,,15,20,25,30
+"""numerouniversal=5
+while numerouniversal <=30:
+    print(numerouniversal, end="")
+    numerouniversal +=5"""
+
+#programa que imprime ,2,3,4,5,6,7,8
+"""numerouniversal=2
+while numerouniversal <= 8:
+    print(numerouniversal, end="")
+    numerouniversal +=2"""
+
+#programa que imprime 1,2,3,4,5,6,7,8
+"""numerouniversal=1
+while numerouniversal <= 8:
+    print(numerouniversal, end="")
+    numerouniversal +=1"""
