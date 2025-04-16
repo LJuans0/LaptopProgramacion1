c91e077ed1c9be9729ad380e948bd7a293e0b71a
Juan <johnstar08082@gmail.com>
2025-04-16 15:16:59 -0500

    15:16

--- April16.py
+++ April16.py
@@ -1,6 +1,12 @@
 #historial de abajo para arriba
 
-#S
+#suma 2**1 + 2**2 + 2**3 + 2**4 + 2**5 + 2**6 + 2**7 + 2**8
+"""i=1
+opr=0
+while i<=100:
+    opr+=i
+    i+=1
+print(opr)"""
 
 #suma 2**1 + 2**2 + 2**3 + 2**4 + 2**5 + 2**6 + 2**7 + 2**8
 """i=1
