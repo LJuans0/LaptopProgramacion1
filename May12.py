0ca11fef66fdfd1434af64bcdee7012cf9db6c70
Juan <johnstar08082@gmail.com>
2025-05-12 11:17:14 -0500

    11:17

---
+++ May12.py
@@ -0,0 +1,49 @@
+def pregunta_1(cristales: list[int]) -> int:
+    """
+    Retorna la cantidad de cristales que Jin-Woo puede usar para abrir la puerta dimensional.
+    Parametros:
+        cristales (list[int]): lista de valores enteros de energia magica.
+    Retorna:
+        int: numero de cristales validos.
+    """
+    return None
+
+def potencial(valor: int) -> int:
+    """
+    Calcula el potencial de energia maligna de un cristal de acuerdo a las reglas establecidas.
+    """
+    return None
+
+def pregunta_2(cristales: list[int]) -> list[int]:
+    """
+    Usa la funcion 'potencial' para evaluar cada cristal y retorna
+    solo aquellos cuyo potencial de energia maligna es mayor a 50.
+    
+    Parametros:
+        cristales (list[int]): lista de valores de los cristales.
+    Retorna:
+        list[int]: cristales cuyo potencial de energia maligna supera 50.
+    """
+    return None
+
+
+def pregunta_3(datos: list[str], nombres: list[str]) -> list[str]:
+    """
+    Verifica si se puede formar un equipo para la operacion en la isla Jeju.
+    Si es posible, retorna los nombres que cumplen los requisitos minimos.
+    Si no, retorna lista vacia.
+    """
+    return None
+
+
+def pregunta_4(objetos: list[int]) -> list[int]:
+    """
+    Modifica los poderes de los objetos segun las reglas de entrenamiento, 
+    y filtra los objetos que no sean multiplos de 3, retornando la lista modificada.
+    Parametros:
+        objetos (list[int]): lista de poderes de los objetos.
+    Retorna:
+        list[int]: lista con los poderes modificados y filtrados.
+    """
+    resultado = []
+    return None
